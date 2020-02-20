a80d088d5371171b0027a1f9edaef33152b91f74
novolife <mahajana@foxmail.com>
2020-02-20 14:28:11 +0800

    add unsaved warning title

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,14 +7,36 @@ MainWindow::MainWindow(QMap<QString, QString> rSet, QWidget *parent)
 {
     ui->setupUi(this);
     ui->sid->setValidator(new QRegularExpressionValidator(QRegularExpression("\\d{13}")));
-/*
-    do
+    report_set = rSet;
+
+    while(1)
     {
-        save_add = QFileDialog::getSaveFileName(this, "新建报告文件", "./untitled.rpt", "报告文件(*.rpt)");
+        save_add = QFileDialog::getSaveFileName(this, "Select Saving Position", "./untitled.rpt", "Report File (*.rpt)");
+        file_info = new QFileInfo(save_add);
+
+        if (save_add == "")
+        {
+            QMessageBox::critical(this, "Please Select Saving Position!", "Report Saving Position Could NOT Be Empty.");
+        }
+        else if (file_info->suffix() != ".rpt")
+        {
+            QMessageBox::critical(this, "Please Use Correct Suffix!", "Report File Suffix MUST BE \".rpt\"");
+        }
+        else
+        {
+            break;
+        }
     }
-    while (save_add == "" && (QMessageBox::critical(this, "文件名不能为空！", "请设置报告文件位置！")));
+
+    file_info = new QFileInfo(save_add);
+    file_name = file_info->fileName();
+    setWindowTitle(file_name + "* - labReport");
+
     save_file = new QFile(save_add);
-    save_file->open(QIODevice::WriteOnly);*/
+    save_file->open(QIODevice::WriteOnly);
+    save_file->close();
+
+    setting_file = new QSettings(save_add, QSettings::IniFormat);
 
     ui->sname->setText(rSet["sName"]);
     ui->sid->setText(rSet["sID"]);
@@ -85,8 +107,11 @@ void MainWindow::on_actionRedo_triggered()
 
 void MainWindow::closeEvent(QCloseEvent *event)
 {
-    if(get_status() == SAVED || get_status() == NOSAVE)
+    if(get_status() == SAVED)
+    {
+        saving_file();
         event->accept();
+    }
     else if (get_status() == UNSAVED)
     {
         unsaved *dialog = new unsaved(this);
@@ -95,27 +120,190 @@ void MainWindow::closeEvent(QCloseEvent *event)
         dialog->show();
         event->ignore();
     }
+    else if (get_status() == NOSAVE)
+    {
+        event->accept();
+    }
 
 }
 
+/**
+ * @brief save the content in textedits
+ */
+void MainWindow::saving_file()
+{
+    setting_file->setValue("profile/sname", report_set["sName"]);
+    setting_file->setValue("profile/sid", report_set["sID"]);
+    setting_file->setValue("profile/cname", report_set["cName"]);
+    setting_file->setValue("profile/etchr", report_set["eTchr"]);
+    setting_file->setValue("profile/ctchr", report_set["cTchr"]);
+    setting_file->setValue("profile/eaddr", report_set["eAddr"]);
+    setting_file->setValue("profile/edate", report_set["eDate"]);
+    setting_file->setValue("profile/eroom", report_set["eRoom"]);
+    setting_file->setValue("profile/ename", report_set["eName"]);
+    setting_file->setValue("profile/ehour", report_set["eHour"]);
+
+    setting_file->setValue("text/eprin", ui->eprin->toPlainText());
+    setting_file->setValue("text/epurp", ui->epurp->toPlainText());
+    setting_file->setValue("text/econt", ui->econt->toPlainText());
+    setting_file->setValue("text/etool", ui->etool->toPlainText());
+    setting_file->setValue("text/estep", ui->estep->toPlainText());
+    setting_file->setValue("text/erslt", ui->erslt->toPlainText());
+    setting_file->setValue("text/ecncl", ui->ecncl->toPlainText());
+    setting_file->setValue("text/efeel", ui->efeel->toPlainText());
+    setting_file->setValue("text/eadvs", ui->eadvs->toPlainText());
+
+    setWindowTitle(save_add + " - labReport");
+}
+
+/**
+ * @brief slot func for about act.
+ */
 void MainWindow::on_actionAbout_triggered()
 {
     about *about_dialog = new about();
     about_dialog->show();
 }
 
+/**
+ * @brief slot func for exit act.
+ */
 void MainWindow::on_actionExit_triggered()
 {
     close();
 }
 
+/**
+ * @brief slot func for unsaved window -> Save
+ */
 void MainWindow::save_unsaved()
 {
-
+    saving_file();
 }
 
+/**
+ * @brief slot func for unsaved window -> Yes
+ */
 void MainWindow::nosave_unsaved()
 {
     set_status(NOSAVE);
     close();
+}
+
+/**
+ * @brief slot func for save act.
+ */
+void MainWindow::on_actionSave_triggered()
+{
+    saving_file();
+}
+
+void MainWindow::unsaved_title()
+{
+    setWindowTitle(save_add + "* - labReport");
+}
+
+void MainWindow::on_etool_textChanged()
+{
+    unsaved_title();
+}
+
+void MainWindow::on_eprin_textChanged()
+{
+    unsaved_title();
+}
+
+void MainWindow::on_epurp_textChanged()
+{
+    unsaved_title();
+}
+
+void MainWindow::on_econt_textChanged()
+{
+    unsaved_title();
+}
+
+void MainWindow::on_estep_textChanged()
+{
+    unsaved_title();
+}
+
+void MainWindow::on_erslt_textChanged()
+{
+    unsaved_title();
+}
+
+void MainWindow::on_ecncl_textChanged()
+{
+    unsaved_title();
+}
+
+void MainWindow::on_efeel_textChanged()
+{
+    unsaved_title();
+}
+
+void MainWindow::on_eadvs_textChanged()
+{
+    unsaved_title();
+}
+
+void MainWindow::on_sname_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    unsaved_title();
+}
+
+void MainWindow::on_sid_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    unsaved_title();
+}
+
+void MainWindow::on_cname_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    unsaved_title();
+}
+
+void MainWindow::on_ctchr_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    unsaved_title();
+}
+
+void MainWindow::on_etchr_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    unsaved_title();
+}
+
+void MainWindow::on_eaddr_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    unsaved_title();
+}
+
+void MainWindow::on_edate_dateChanged(const QDate &date)
+{
+    Q_UNUSED(date)
+    unsaved_title();
+}
+
+void MainWindow::on_eroom_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    unsaved_title();
+}
+
+void MainWindow::on_ename_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    unsaved_title();
+}
+
+void MainWindow::on_ehour_textChanged(const QString &arg1)
+{
+    Q_UNUSED(arg1)
+    unsaved_title();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -36,15 +36,48 @@ private slots:
     void on_actionRedo_triggered();
     void on_actionAbout_triggered();
     void on_actionExit_triggered();
+    void on_actionSave_triggered();
 
     void save_unsaved();
     void nosave_unsaved();
 
+    void on_etool_textChanged();
+    void on_eprin_textChanged();
+    void on_epurp_textChanged();
+    void on_econt_textChanged();
+    void on_estep_textChanged();
+    void on_erslt_textChanged();
+    void on_ecncl_textChanged();
+    void on_efeel_textChanged();
+    void on_eadvs_textChanged();
+
+    void on_sname_textChanged(const QString &arg1);
+    void on_sid_textChanged(const QString &arg1);
+    void on_cname_textChanged(const QString &arg1);
+    void on_ctchr_textChanged(const QString &arg1);
+    void on_etchr_textChanged(const QString &arg1);
+    void on_eaddr_textChanged(const QString &arg1);
+    void on_edate_dateChanged(const QDate &date);
+    void on_eroom_textChanged(const QString &arg1);
+    void on_ename_textChanged(const QString &arg1);
+    void on_ehour_textChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
+
+    QMap<QString, QString> report_set;
+
     QFile *save_file = nullptr;
+    QFileInfo *file_info = nullptr;
+    QSettings *setting_file = nullptr;
+
     QString save_add = "";
+    QString file_name = "";
+
     status report_s = UNSAVED;
+
+    void saving_file();
+    void unsaved_title();
 
 };
 #endif // MAINWINDOW_H
